02f98eee9ad6a61da99b91f1a9590f2b765c822f
Jonathan Verrecchia <jonathan.verrecchia@gmail.com>
2019-09-04 23:35:14 +0900

    Add check, checkPresent, checkAbsent

---
+++ check.d.ts
@@ -0,0 +1,3 @@
+export function check(envVarName: string, predicate: Function, errorMessageFn?: Function): boolean
+export function checkPresent(...envVarName: string[]): boolean
+export function checkAbsent(...envVarName: string[]): boolean
